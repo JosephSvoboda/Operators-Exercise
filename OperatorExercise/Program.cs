14b48d3220fe587549e50736eef728c88feb6fb6
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-10-17 15:20:30 -0400

    made some operators, and make a raidus calc method

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+
+            int b = 4;
+
+            int add = a + b;
+
+            int sub = a - b;
+
+            int mult = a * b;
+
+            int quotient = a / b;
+
+            int remainder = a % b;
+
+                Console.WriteLine($"{a}/{b} is {b} remainder {remainder}");
+                Console.WriteLine();
+
+            double AreaOfCircle(double r)
+            {
+                double radius = Math.PI * Math.Pow(r, 2);
+                return radius;
+            }
+
+                Console.Write("What is the radius of your circle?:");
+
+            double radius = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"The area of a circle with the radius of {radius} is {AreaOfCircle(radius)}");
         }
     }
 }
